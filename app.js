16149847875d0fa81fd2c38db65bfae133723cdb
Nabeel Ahmed <nabeelahmed1721@gmail.com>
2020-04-14 18:29:12 -0400

    fixed bugs

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const generateMono = require('./src/generateMono')
 /* 
 generateMono(IMAGE_LENGTH_IN_PIXELS, OUTPUT_LOCATION)
 -> DEMO:
-        generateMono(2, __dirname)
+        generateMono(2, some_directory)
 -> OUTPUT:
         ◻◻
         ◻◻
 
-        ANYTHING OVER 4 length needs 4 GB of RAM to compute!
+        ANYTHING OVER 3 length needs 4 GB of RAM to compute!
         use: node --max-old-space-size=4096 app.js to use more RAM
 */
 
 
-generateMono(2, __dirname+"/monochrome/")
+generateMono(4, __dirname+"/monochrome/")
